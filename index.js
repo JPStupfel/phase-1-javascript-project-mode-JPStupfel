6c364ff8d3fb76e1b59c94647e1e86829d3131e6
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-05-06 12:43:56 -0500

    fetch is working

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const test = document.createElement('div')
 test.textContent = 'testing'
 document.querySelector('body').appendChild(test)
 
-fetch('https://api.coingecko.com/api/v3/exchange_rates').then(res=>res.json()).then(data=> {test.textContent = data['rates']['btc']['value']; console.log(data)})
+fetch('https://api.coingecko.com/api/v3/exchange_rates').then(res=>res.json()).then(data=> {test.textContent = data['rates']['aed']['value']; console.log(data)})
+
