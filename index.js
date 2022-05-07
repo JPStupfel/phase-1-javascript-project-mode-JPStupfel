6a7efd54a8e1ea9a4a04058177d7c20d7c9db554
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-05-06 19:13:38 -0500

    added DOMContentLoaded event listener to the first fetch

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
+document.addEventListener('DOMContentLoaded',
 
-fetch('https://api.coingecko.com/api/v3/exchange_rates').then(res=>res.json()).then(data=> { for (let i of Object.keys(data['rates']))
-    { 
-        makeCard.call(data['rates'][i],i);
-    }
-})
-
+()=>{fetch('https://api.coingecko.com/api/v3/exchange_rates').then(res=>res.json()).then(data=> {
+        for (let i of Object.keys(data['rates'])){
+                makeCard.call(data['rates'][i],i)}
+})}
+)
 
 
 function makeCard(id){
@@ -54,3 +54,11 @@ function makeCard(id){
         }
     )
 }
+
+
+//provide a function to sort by total value, lowest and another sort by highest
+
+//provide a function to switch base rate
+    //this could be done by adding a global variable for base rate, then on pValue in makeCard add an event listener that listens for global variable base rate to change...and when it does it changes it's function scope variable
+
+    //or...I could add a second arg to makeCard that is the exchange rate, and when you click the base rate button of a certain card, it 1. dels all the active cards, 2. runs make card again with the new base rate.
