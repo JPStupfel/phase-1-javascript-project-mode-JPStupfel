83e9f9bd73346556453e54da945fb17197f738aa
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-05-07 12:57:44 -0500

    time stamp is shown

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 
 let jsonOBJ = {}
 let globalBaserate = 1
+let timeStamp
 document.addEventListener('DOMContentLoaded',
 
 ()=>{fetch('https://api.coingecko.com/api/v3/exchange_rates').then(res=>res.json()).then(data=> {
-        buildPage(data); jsonOBJ = data;
+        buildPage(data); jsonOBJ = data; timeStamp = new Date(); addTimeStamp(timeStamp)
+
 })}
 )
 
+function addTimeStamp(time){
+    let timeHeader = document.createElement('h1')
+    timeHeader.id = 'time-header'
+    timeHeader.textContent = `This request pulled from api.coingecko.com on ${time}`
+    document.querySelector('body').prepend(timeHeader)
 
-//separate the for loop in the fetch funcion. make it take the json object.
-//copy the json object so it can be reused
+}
+
 
 function buildPage(data){
     //start by clearing the dom
-    while (document.querySelector('body').children.length) {document.querySelector('body').firstChild.remove()}
+    while (document.querySelector('#card-body').children.length) {document.querySelector('#card-body').firstChild.remove()}
     //then build all the cards
     for (let i of Object.keys(data['rates'])){
         makeCard.call(data['rates'][i],i,globalBaserate);
@@ -43,7 +50,7 @@ function makeCard(id,baserate){
     div.appendChild(pName)
     div.appendChild(pValue)
 
-    document.querySelector('body').appendChild(div)
+    document.querySelector('#card-body').appendChild(div)
 
     //make the drop down p
     const divDropDown = document.createElement('div')
@@ -78,26 +85,14 @@ function makeCard(id,baserate){
         }
     )
 
-    
-    baseRateButton.addEventListener('click', ()=> {globalBaserate = this.value; console.log(globalBaserate) ; buildPage(jsonOBJ)}
+    //baserate button resests the global baserate value and rebuilds the page using the jsonOBJ copy
+    baseRateButton.addEventListener('click', ()=> {globalBaserate = this.value; buildPage(jsonOBJ)}
     )
 }
 
-// ()=> {while (document.querySelector('body').children.length) {document.querySelector('body').firstChild.remove()}}
-
-//1. put the fetch request in its own function that makes a global copy of the json file, and makes a global copy of a timestamp.
-//2. then you can call makecard with that global json copy as the context and use Object.keys(this.rates), you won't have to pass anything into makeCard at all.
-//then make makeCard recursive delete all the dom divs if they're there so it can be used to refresh the list when you sort or change the base rate.
-
-//actually, all you have to do is pass the whole json object as context in your first fetch>makeCard call...tehn refactor makeCard using all that data...this.rate>keys etc. 
-//then make makeCards revursive del elems if there, and add makeCard to the button with baserate variable.
-
-
-//provide a function to sort by total value, lowest and another sort by highest
-
-//provide a function to switch base rate
-    //this could be done by adding a global variable for base rate, then on pValue in makeCard add an event listener that listens for global variable base rate to change...and when it does it changes it's function scope variable
-
-    //or...I could add a second arg to makeCard that is the exchange rate, and when you click the base rate button of a certain card, it 1. dels all the active cards, 2. runs make card again with the new base rate.
 
 //display time of fetch, using event listener time get or whatever, at top of screen...with a button to update...but that keeps the current base rate :-)
+
+//I really want the baserate button to be on the right hand side
+
+//if I could refactor makecard somehow..you refactors can be called using dot call and pass this as context
